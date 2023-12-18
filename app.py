bb6f54e516d460fa6029a8ac31b3484beaae4b50
Sahil125kr <sahil125kumar@gmail.com>
2023-12-18 16:31:06 +0530

    add css

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
 
-from flask import Flask
- 
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
  
 
 @app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello Sahil'
- 
+def index():
+    if request.method == 'GET':
+        print("Its in get")
+        print("1")
+        return render_template("index.html")
+    else:
+        print("2")
+        return render_template('Dashboard.html')
+    
+@app.route("/index")
+def ind():
+
+   return render_template("index.html")
+
+@app.route('/about', methods = ['GET', 'POST'])
+def about():
+    print("page open")
+    return render_template('about.html')
+
+@app.route('/courses', methods = ['GET', 'POST'])
+def courses():
+    print("page open")
+    return render_template('Courses.html')
+
+
+@app.route('/contact', methods = ['GET', 'POST'])
+def contact():
+    print("page open")
+    return render_template('Contact.html')
+
+@app.route('/signup',methods=['GET','POST'])
+def signup(): 
+    
+        
+        return render_template('signup.html')
+
+
+
